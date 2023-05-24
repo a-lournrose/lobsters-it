95db89a22f082c645fa052d035487a16b1854a2e
qshssewww <T1A1arxFpzP$>
2023-05-24 15:10:23 +0300

    slider responsiveness (in progress)

--- src/UI/Slider/Slider.jsx
+++ src/UI/Slider/Slider.jsx
@@ -6,28 +6,31 @@ import stl from './Slider.module.scss'
 const Slider = ({inSliders}) => {
 
     return (
-        <Swiper
-            className={stl.slider}
-            spaceBetween={24}
-        >
-            {inSliders.map(slider => (
-                <SwiperSlide key={slider.title} className={stl.slider_item}>
-                    <div className={stl.slider_leftBlock}>
-                        <div className={stl.slider_text}>
-                            <h3>{slider.title}</h3>
-                            <div className={stl.slider_line}></div>
-                            <p>{slider.subTitle}</p>
+        <div style={{overflow: "hidden"}}>
+            <Swiper
+                slidesPerView={1}
+                className={stl.slider}
+                spaceBetween={30}
+            >
+                {inSliders.map(slider => (
+                    <SwiperSlide key={slider.title} className={stl.slider_item}>
+                        <div className={stl.slider_leftBlock}>
+                            <div className={stl.slider_text}>
+                                <h3>{slider.title}</h3>
+                                <div className={stl.slider_line}></div>
+                                <p>{slider.subTitle}</p>
+                            </div>
+                            <div className={stl.slider_btn}>
+                                поехать !
+                            </div>
                         </div>
-                        <div className={stl.slider_btn}>
-                            поехать !
+                        <div className={stl.slider_rightBlock}>
+                            <img src={slider.img} alt="img"/>
                         </div>
-                    </div>
-                    <div className={stl.slider_rightBlock}>
-                        <img src={slider.img} alt="img"/>
-                    </div>
-                </SwiperSlide>
-            ))}
-        </Swiper>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </div>
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import Store from "./store/store";
 import {BrowserRouter} from "react-router-dom";
 import 'antd/dist/reset.css';
+import './styles/index.scss'
 
 const store = new Store();
 
