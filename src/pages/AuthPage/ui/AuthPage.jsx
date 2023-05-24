9594dfe071499530600dd1a09e09720cbb1b394e
qshssewww <T1A1arxFpzP$>
2023-05-24 17:43:17 +0300

    slider responsiveness (in progress)

--- src/pages/AuthPage/ui/AuthPage.jsx
+++ src/pages/AuthPage/ui/AuthPage.jsx
@@ -4,12 +4,14 @@ import Slider from "../../../UI/Slider/Slider";
 const AuthPage = () => {
 
     return (
-        <Slider inSliders={[
-            {title: 'Санкт-Петербург', subTitle: 'Я люблю это небо за смелость быть голубым сквозь серость.', img: '/SPB.png'},
-            {title: 'Москва', subTitle: 'Москва ... впцу.', img: '/SPB.png'},
-            {title: 'avasd', subTitle: '1232222', img: '/SPB.png'}
-        ]}>
-        </Slider>
+        <div>
+            <Slider inSliders={[
+                {title: 'Санкт-Петербург', subTitle: 'Я люблю это небо за смелость быть голубым сквозь серость.', img: '/SPB.png'},
+                {title: 'Москва', subTitle: 'Москва ... впцу.', img: '/SPB.png'},
+                {title: 'avasd', subTitle: '1232222', img: '/SPB.png'}
+            ]}>
+            </Slider>
+        </div>
     );
 };
 
