f7088f1fa4a953356141cca1412f5f53c75cefa9
qshssewww <T1A1arxFpzP$>
2023-05-27 23:43:44 +0300

    card + delete public robots

---
+++ src/UI/ButtonBuy/ButtonBuy.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import stl from './ButtonBuy.module.scss'
+const ButtonBuy = () => {
+    return (
+        <div className={`${stl.button} flex-center`}>
+            Купить
+        </div>
+    );
+};
+
+export default ButtonBuy;
